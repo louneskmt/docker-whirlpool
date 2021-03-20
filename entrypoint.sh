6929db5150d0fe8079256535dec0167e427cdbd1
Lounès Ksouri <dev@louneskmt.com>
2021-03-20 14:21:48 +0100

    Add network config in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,14 @@ whirlpool_options=(
   --cli.torConfig.executable=/usr/local/bin/tor
 )
 
+WHIRLPOOL_BITCOIN_NETWORK=$(printf '%s\n' "$WHIRLPOOL_BITCOIN_NETWORK" | awk '{ print toupper($0) }')
+
+if [ "$WHIRLPOOL_BITCOIN_NETWORK" == "TESTNET" ]; then
+  whirlpool_options+=(--cli.server="TESTNET")
+else
+  whirlpool_options+=(--cli.server="MAINNET")
+fi
+
 if [ "$WHIRLPOOL_DOJO" == "on" ]; then
   if [[ -z WHIRLPOOL_DOJO_IP ]]; then
     echo "Error: WHIRLPOOL_DOJO_IP is undefined."
@@ -13,7 +21,7 @@ if [ "$WHIRLPOOL_DOJO" == "on" ]; then
 
   whirlpool_options+=(--cli.dojo.enabled=true)
 
-  if [ "$WHIRLPOOL_BITCOIN_NETWORK" == "testnet" ]; then
+  if [ "$WHIRLPOOL_BITCOIN_NETWORK" == "TESTNET" ]; then
     whirlpool_options+=(--cli.dojo.url="http://$WHIRLPOOL_DOJO_IP:80/test/v2/")
   else
     whirlpool_options+=(--cli.dojo.url="http://$WHIRLPOOL_DOJO_IP:80/v2/")
@@ -21,4 +29,4 @@ if [ "$WHIRLPOOL_DOJO" == "on" ]; then
 fi
 
 cd /home/whirlpool/.whirlpool-cli
-java -jar /usr/local/whirlpool-cli/whirlpool-client-cli-run.jar "--listen ${whirlpool_options[@]} $@"
+java -jar /usr/local/whirlpool-cli/whirlpool-client-cli-run.jar "$@ ${whirlpool_options[@]}"
